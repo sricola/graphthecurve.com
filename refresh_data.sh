5927b58bc6a73923b9b1d1b088f707fa9c21807c
Sri Ray <hi@sri.so>
2020-03-26 20:02:46 -0400

    refreshed data

--- refresh_data.sh
+++ refresh_data.sh
@@ -2,8 +2,6 @@
 
 cd data/
 
-rm *.csv
-
 wget -O time_series_covid19_confirmed_global.csv.$(date +%F) https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 wget -O time_series_covid19_deaths_global.csv.$(date +%F) https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
 
